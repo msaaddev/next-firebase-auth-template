7a43ffccf6957882315bf6a9f1042b763dad571b
msaaddev <mrsaadirfan@gmail.com>
2021-09-12 16:33:38 +0500

    📦 NEW: firebase config

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+	apiKey: `${process.env.NEXT_PUBLIC_Firebase_API_Key}`,
+	authDomain: `${process.env.NEXT_PUBLIC_Auth_Domain}`,
+	projectId: `${process.env.NEXT_PUBLIC_Project_Id}`,
+	storageBucket: `${process.env.NEXT_PUBLIC_Storage_Bucket}`,
+	messagingSenderId: `${process.env.NEXT_PUBLIC_Message_Sender_Id}`,
+	appId: `${process.env.NEXT_PUBLIC_App_Id}`
+};
+
+const fire = initializeApp(config);
+
+export default fire;
